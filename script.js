5f135655731308605e8ccafb74287f76fc59b7e9
Abdo2711-coder-two <a60960764@gmail.com>
2025-02-03 05:24:33 +0200

    .

--- script.js
+++ script.js
@@ -6,57 +6,57 @@ const path = "./data.json";
 const git = simpleGit();
 
 // Define coordinates for each letter (ABDO on left, ELMORSI on right)
-// Adjusted coordinates to center the text
+// Adjusted y-coordinates by adding 1
 const letters = {
     "A": [
-        [1, 2], [1, 3], [1, 4], [1, 5],  // Left vertical
-        [5, 2], [5, 3], [5, 4], [5, 5],  // Right vertical
-        [2, 1], [3, 1], [4, 1],          // Top horizontal
-        [3, 3], [4, 3]                   // Middle horizontal
+        [1, 3], [1, 4], [1, 5], [1, 6],  // Left vertical
+        [5, 3], [5, 4], [5, 5], [5, 6],  // Right vertical
+        [2, 2], [3, 2], [4, 2],          // Top horizontal
+        [3, 4], [4, 4]                   // Middle horizontal
     ],
     "B": [
-        [7, 1], [7, 2], [7, 3], [7, 4], [7, 5],  // Left vertical
-        [8, 1], [9, 1], [10, 2], [9, 3], [10, 4], [9, 5], [8, 5] // Right & curves
+        [7, 2], [7, 3], [7, 4], [7, 5], [7, 6],  // Left vertical
+        [8, 2], [9, 2], [10, 3], [9, 4], [10, 5], [9, 6], [8, 6] // Right & curves
     ],
     "D": [
-        [12, 1], [12, 2], [12, 3], [12, 4], [12, 5], // Left vertical
-        [13, 1], [14, 2], [15, 3], [14, 4], [13, 5]  // Curved right side
+        [12, 2], [12, 3], [12, 4], [12, 5], [12, 6], // Left vertical
+        [13, 2], [14, 3], [15, 4], [14, 5], [13, 6]  // Curved right side
     ],
     "O": [
-        [17, 2], [17, 3], [17, 4], [17, 5], // Left vertical
-        [21, 2], [21, 3], [21, 4], [21, 5], // Right vertical
-        [18, 1], [19, 1], [20, 1],         // Top horizontal
-        [18, 6], [19, 6], [20, 6]          // Bottom horizontal
+        [17, 3], [17, 4], [17, 5], [17, 6], // Left vertical
+        [21, 3], [21, 4], [21, 5], [21, 6], // Right vertical
+        [18, 2], [19, 2], [20, 2],         // Top horizontal
+        [18, 7], [19, 7], [20, 7]          // Bottom horizontal
     ],
     "E": [
-        [26, 1], [26, 2], [26, 3], [26, 4], [26, 5], // Left vertical
-        [27, 1], [28, 1], [29, 1],                // Top horizontal
-        [27, 3], [28, 3], [29, 3],                // Middle horizontal
-        [27, 5], [28, 5], [29, 5]                 // Bottom horizontal
+        [26, 2], [26, 3], [26, 4], [26, 5], [26, 6], // Left vertical
+        [27, 2], [28, 2], [29, 2],                // Top horizontal
+        [27, 4], [28, 4], [29, 4],                // Middle horizontal
+        [27, 6], [28, 6], [29, 6]                 // Bottom horizontal
     ],
     "L": [
-        [31, 1], [31, 2], [31, 3], [31, 4], [31, 5], // Left vertical
-        [32, 5], [33, 5], [34, 5]                 // Bottom horizontal
+        [31, 2], [31, 3], [31, 4], [31, 5], [31, 6], // Left vertical
+        [32, 6], [33, 6], [34, 6]                 // Bottom horizontal
     ],
     "M": [
-        [36, 1], [36, 2], [36, 3], [36, 4], [36, 5], // Left vertical
-        [40, 1], [40, 2], [40, 3], [40, 4], [40, 5], // Right vertical
-        [37, 2], [38, 3], [39, 2]                     // Middle connections
+        [36, 2], [36, 3], [36, 4], [36, 5], [36, 6], // Left vertical
+        [40, 2], [40, 3], [40, 4], [40, 5], [40, 6], // Right vertical
+        [37, 3], [38, 4], [39, 3]                     // Middle connections
     ],
     "R": [
-        [42, 1], [42, 2], [42, 3], [42, 4], [42, 5], // Left vertical
-        [43, 1], [44, 1], [45, 2], [44, 3], [43, 3], // Top and mid curve
-        [44, 4], [45, 5]                            // Leg of R
+        [42, 2], [42, 3], [42, 4], [42, 5], [42, 6], // Left vertical
+        [43, 2], [44, 2], [45, 3], [44, 4], [43, 4], // Top and mid curve
+        [44, 5], [45, 6]                            // Leg of R
     ],
     "S": [
-        [47, 1], [48, 1], [49, 1],       // Top horizontal
-        [47, 2],                         // Left vertical
-        [47, 3], [48, 3], [49, 3],       // Middle horizontal
-        [49, 4],                         // Right vertical
-        [47, 5], [48, 5], [49, 5]        // Bottom horizontal
+        [47, 2], [48, 2], [49, 2],       // Top horizontal
+        [47, 3],                         // Left vertical
+        [47, 4], [48, 4], [49, 4],       // Middle horizontal
+        [49, 5],                         // Right vertical
+        [47, 6], [48, 6], [49, 6]        // Bottom horizontal
     ],
     "I": [
-        [51, 1], [51, 2], [51, 3], [51, 4], [51, 5]  // Vertical bar
+        [51, 2], [51, 3], [51, 4], [51, 5], [51, 6]  // Vertical bar
     ]
 };
 
@@ -70,7 +70,7 @@ const points = [
 // Function to generate a commit date starting from 3 years ago
 const getCommitDate = (x, y) => {
     return moment()
-        // .subtract(2, "y") // Go back 3 years
+        .subtract(3, "y") // Go back 3 years
         .startOf('year')  // Start from the beginning of the year
         .add(x, "w")      // Move across weeks
         .add(y, "d")      // Move within week
